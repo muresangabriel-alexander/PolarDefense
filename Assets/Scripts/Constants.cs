4229f6b294c27c25038dd869b27bcbba7f4028f6
Gabriel Alexandru Muresan <gabriel.muresan@student.tugraz.at>
2022-12-04 11:38:26 +0100

    created base scene objects

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants : MonoBehaviour
+{
+  public static readonly string TUNNEL_ENTRY = "TUNNEL_ENTRY";
+  public static readonly string TUNNEL_EXIT = "TUNNEL_EXIT";
+  public static readonly string ICE_SHARDS = "ICE_SHARDS";
+  public static readonly string WAYPOINT = "WAYPOINT";
+  public static readonly string FINISH_LINE = "FINISH_LINE";
+  public static readonly string PLATFORM = "PLATFORM";
+
+}
