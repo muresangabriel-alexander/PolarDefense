60fc3910db22a5aa1db0ccfc262fc2649c055dfc
CarolinSchaffer <carolinschaffer6@gmail.com>
2023-02-12 22:13:11 +0100

    added animation for water tower

--- Assets/Scripts/WaterTowerScript.cs
+++ Assets/Scripts/WaterTowerScript.cs
@@ -6,14 +6,23 @@ public class WaterTowerScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    public Sprite tower1;
+    public Sprite tower2;
+    public Sprite tower3;
+    public Sprite tower4;
+
     private float cooldownTime;
     private float waterFloodedCountdown;
     private bool isFlooded;
-        private void Awake()
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
     {
     }
     void Start()
     {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = tower1;
         isFlooded = false;
         cooldownTime = 0.0f;
         waterFloodedCountdown = Constants.WATER_TOWER_FLOODING_RATE;
@@ -46,11 +55,27 @@ public class WaterTowerScript : MonoBehaviour
     {
         if(canAttack)
         {
+            StartCoroutine(wait());
             cooldownTime = Constants.WATER_TOWER_FLOODING_RATE;
             transform.GetChild(0).gameObject.SetActive(true);
             isFlooded = true;
             cooldownTime = Constants.WATER_TOWER_FLOODING_RATE;
         }
+    }
+
+    IEnumerator wait()
+    {
+        spriteRenderer.sprite = tower2;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.sprite = tower3;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.sprite = tower4;
+        yield return new WaitForSeconds(0.3f);
+        spriteRenderer.sprite = tower3;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.sprite = tower2;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.sprite = tower1;
     }
 
     public bool canAttack
