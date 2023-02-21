ff66322dce51f83022078644dfea5d7e5e7f4160
Tk1307 <58664630+Tk1307@users.noreply.github.com>
2023-02-21 16:54:02 +0100

    Finalscene, Highscore System basic and fix for healthbar init value
    Implemented a Final scene, A highscore system and a fix for the issue that doesn't reset the healthbar

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class playerScript : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class playerScript : MonoBehaviour
     [SerializeField]
     private StatusBoardSO statusBoardObject;
     public static int hunger = 100;
+    [SerializeField]
+    private HighscoreScriptable highscoreObject; 
 
     public SpriteRenderer spriteRenderer;
     public Sprite newSprite;
@@ -72,7 +75,10 @@ public class playerScript : MonoBehaviour
     void Update()
     {
         if (HealthBarScript.currentHealth <= 0)
+        {
             Destroy(gameObject);
+            SceneManager.LoadScene("FinalScene");
+        }
     }
 
     private void FixedUpdate()
@@ -100,6 +106,10 @@ public class playerScript : MonoBehaviour
 
     private void OnDestroy()
     {
-        // TODO exit game here
+        if(statusBoardObject.GetDestroyedVehicles() > highscoreObject.Highscore)
+        {
+            highscoreObject.Highscore = statusBoardObject.GetDestroyedVehicles();
+        }
+        highscoreObject.CurrentScore = statusBoardObject.GetDestroyedVehicles();
     }
 }
